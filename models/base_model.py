e1dd95c9b3e13d176ae4c8a44cd1d2f1ef26863e
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-27 15:16:55 -0700

    Create BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,19 @@ class BaseModel:
         """
         return a repersentation of the instance.
         """
-        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
 
     def save(self):
         """
-        Update the public instance attribute updated_at with the current datetime.
+        Update the public instance attribute updated_at with the
+        current datetime.
         """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance.
+        returns a dictionary containing all keys/values of __dict__
+        of the instance.
         """
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
