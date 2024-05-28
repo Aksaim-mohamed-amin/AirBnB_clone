2c0b5a38f6dc28aafe49326610563d256ebad87d
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-28 03:38:01 -0700

    Create BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,31 @@
 """ Base Model """
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """ Base model class """
     def __init__(self):
         """
-        base model constructur.
+        base model constructor.
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
         return a repersentation of the instance.
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
         Update the public instance attribute updated_at with the
         current datetime.
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
